38823007a820243c825dba55838eec7667c1607a
khuzaimashoaib <khuzaima.shoaib@gmail.com>
2023-02-06 21:56:22 +0500

    ibmi

---
+++ ibmi/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:ibmi/pages/main_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      title: 'IBMI',
+      initialRoute: '/',
+      routes: {
+        '/':(context) => const MainPage(),
+          
+        
+      },
+    );
+  }
+}
